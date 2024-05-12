dadf669f170f3230e2b5668d1ecf5b4f397e3f93
DONOTINTO <goods99j@gmail.com>
2024-05-12 22:59:07 +0900

    Init: 프로젝트 생성 및 기본 파일 폴더링

---
+++ YouthTalkTalk/YouthTalkTalk/Present/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouthTalkTalk
+//
+//  Created by 이중엽 on 5/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
