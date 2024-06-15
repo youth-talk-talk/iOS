76275f462d77d0a8732d931eb0ed4de1183bb862
DONOTINTO <goods99j@gmail.com>
2024-06-15 21:37:13 +0900

    Feat: 약관동의 타이틀 설정

--- YouthTalkTalk/YouthTalkTalk/Present/Terms/TermsViewController.swift
+++ YouthTalkTalk/YouthTalkTalk/Present/Terms/TermsViewController.swift
@@ -11,10 +11,7 @@ class TermsViewController: BaseViewController<TermsView> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.black, .font: UIFont(name: "Pretendard-Bold", size: 18) ?? .systemFont(ofSize: 18)]
         
-        self.navigationItem.title = "진짜"
+        updateNavigationTitle(title: "약관동의")
     }
-    
 }
