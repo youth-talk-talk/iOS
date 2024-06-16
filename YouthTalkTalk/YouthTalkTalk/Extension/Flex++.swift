bbb7caf03ef4d867b88e7b235e6fa4da9fc8ec05
DONOTINTO <goods99j@gmail.com>
2024-06-16 15:59:31 +0900

    Feat: Flex return으로인한 경고 문구 삭제

--- YouthTalkTalk/YouthTalkTalk/Extension/Flex++.swift
+++ YouthTalkTalk/YouthTalkTalk/Extension/Flex++.swift
@@ -10,22 +10,26 @@ import FlexLayout
 
 extension Flex {
     
+    @discardableResult
     func defaultButtonCornerRadius(_ radius: CGFloat = 8) -> Flex {
         
         self.cornerRadius(radius)
     }
     
+    @discardableResult
     func defaultButtonHeight(_ height: CGFloat = 48) -> Flex {
         
         self.height(height)
     }
     
+    @discardableResult
     func defaultButton() -> Flex {
         
         self.defaultButtonHeight()
             .defaultButtonCornerRadius()
     }
     
+    @discardableResult
     func signInButton() -> Flex {
         
         self.height(54)
