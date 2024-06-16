728d0620aa7a6684a2fa4d815ba97b4e16b3fd55
DONOTINTO <goods99j@gmail.com>
2024-06-16 15:59:06 +0900

    Feat: 버튼 Configure 기능 추가

---
+++ YouthTalkTalk/YouthTalkTalk/Extension/UIButton++.swift
@@ -0,0 +1,67 @@
+//
+//  UIButton++.swift
+//  YouthTalkTalk
+//
+//  Created by 이중엽 on 6/16/24.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    // 이미지 버튼
+    func designedByImage(_ image: UIImage) {
+        
+        var buttonConfiguration = UIButton.Configuration.plain()
+        buttonConfiguration.background.image = image
+        
+        self.configuration = buttonConfiguration
+        updateHandler(bgColor: .clear)
+    }
+    
+    // 기본 버튼 디자인
+    func designed(title: String, titleColor: UIColor = .black, bgColor: UIColor = .lime40, fontType: FontType = .bodyRegular) {
+        
+        var titleAttrribute = AttributedString.init(title)
+        titleAttrribute.font = FontManager.font(fontType)
+        
+        var buttonConfiguration = UIButton.Configuration.plain()
+        buttonConfiguration.title = title
+        buttonConfiguration.attributedTitle = titleAttrribute
+        buttonConfiguration.baseBackgroundColor = bgColor
+        buttonConfiguration.baseForegroundColor = titleColor
+        buttonConfiguration.background.cornerRadius = 8
+        
+        self.configuration = buttonConfiguration
+        
+        updateHandler(bgColor: bgColor)
+    }
+    
+    // 버튼 업데이트 핸들러
+    private func updateHandler(bgColor: UIColor) {
+        
+        let updateHandler: UIButton.ConfigurationUpdateHandler = { btn in
+            
+            switch btn.state {
+            case .disabled:
+                btn.configuration?.background.backgroundColor = .systemGray
+            case .highlighted:
+                btn.animate()
+            default:
+                btn.configuration?.background.backgroundColor = bgColor
+            }
+        }
+        
+        self.configurationUpdateHandler = updateHandler
+    }
+    
+    // 버튼 애니메이션
+    private func animate() {
+        
+        UIButton.animate(withDuration: 0.03, delay: 0, options: .autoreverse , animations: {
+            self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        }, completion: { _ in
+            self.transform = CGAffineTransform(scaleX: 1, y: 1)
+        })
+    }
+}
