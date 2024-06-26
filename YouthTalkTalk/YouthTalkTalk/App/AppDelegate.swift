392a62d53bd94d868522c13f32499594d3dfb60f
DONOTINTO <goods99j@gmail.com>
2024-06-26 19:11:10 +0900

    Feat: 카카오 로그인 SDK 초기화 및 사전설정

--- YouthTalkTalk/YouthTalkTalk/App/AppDelegate.swift
+++ YouthTalkTalk/YouthTalkTalk/App/AppDelegate.swift
@@ -6,6 +6,12 @@
 //
 
 import UIKit
+import RxKakaoSDKCommon
+import RxKakaoSDKAuth
+import RxKakaoSDKUser
+import KakaoSDKCommon
+import KakaoSDKAuth
+import KakaoSDKUser
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +20,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let nativeAppKey: String = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] as? String ?? ""
+        RxKakaoSDK.initSDK(appKey: nativeAppKey)
+        
         return true
+    }
+    
+    // 카카오톡으로 로그인 기능을 구현하기 위한 필수 설정
+    // 카카오톡에서 서비스 앱으로 돌아왔을 때 카카오 로그인 처리를 정상적으로 완료하기 위해 handleOpenUrl()을 추가
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.rx.handleOpenUrl(url: url)
+        }
+        
+        return false
     }
 
     // MARK: UISceneSession Lifecycle
