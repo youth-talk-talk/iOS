59e29d31379a012f6758aadc3457eba2f15ba344
yououjin <71479613+Y00ujin@users.noreply.github.com>
2024-11-06 19:40:59 +0900

    refactor: [#596] tap 이벤트 추가가능한 코드 추가, 로고 이미지 추가

---
+++ YouthTalkTalk/YouthTalkTalk/Presentation/Util/Extension/UIView+.swift
@@ -0,0 +1,63 @@
+//
+//  UIView+.swift
+//  YouthTalkTalk
+//
+//  Created by 김유진 on 11/6/24.
+//
+
+import UIKit
+
+extension UIView {
+    public func onTapped(_ handler: (() -> Void)?) {
+        self.isUserInteractionEnabled = true
+        self.tapGestureRecognizerHandler = handler
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                          action: #selector(handleTapGesture))
+        self.addGestureRecognizer(tapGestureRecognizer)
+    }
+}
+
+extension UIView {
+    typealias GestureHandler = (() -> Void)?
+    
+    private struct GestureAssociatedKey {
+        /// Objective-C의 연관 객체 기능에서 고유한 주소를 키로 사용하기 위함
+        /// 연관 객체를 저장하거나 검색할 때 사용되는 키 값, 실제 값이 아닌 변수의 주소가 중요
+        ///
+        /// UInt8 변수를 선언하고 초기값으로 0을 할당하는 것은, 이 변수의 주소를 고유한 키로 사용하기 위한 간단한 방법
+        /// UInt8 타입은 메모리를 많이 차지하지 않으면서도 주소를 제공할 수 있는 타입
+        fileprivate static var tapGestureKey: UInt8 = 0
+    }
+    
+    private var tapGestureRecognizerHandler: GestureHandler? {
+        get {
+            return objc_getAssociatedObject(self,
+                                            &GestureAssociatedKey.tapGestureKey) as? GestureHandler
+        }
+        set {
+            if let newValue = newValue {
+                objc_setAssociatedObject(
+                    self,
+                    &GestureAssociatedKey.tapGestureKey,
+                    newValue,
+                    .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+                )
+            }
+        }
+    }
+    
+    @objc private func handleTapGesture(sender: UITapGestureRecognizer) {
+        if let action = self.tapGestureRecognizerHandler, alpha == 1 {
+            alpha = 0.5
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+                action?()
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [weak self] in
+                self?.alpha = 1.0
+            }
+        }
+    }
+    
+}
