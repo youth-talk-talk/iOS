f5af871f85db95d15ac2cfddd0ec2d8b9dd22083
yououjin <71479613+Y00ujin@users.noreply.github.com>
2024-11-21 20:17:51 +0900

    refactor: 게시글 작성 > 정책 검색 API 연동

--- YouthTalkTalk/YouthTalkTalk/Presentation/Present/NewPostView/SearchPolicyView.swift
+++ YouthTalkTalk/YouthTalkTalk/Presentation/Present/NewPostView/SearchPolicyView.swift
@@ -7,8 +7,25 @@
 
 import UIKit
 import SnapKit
+import RxSwift
 
-final class SearchPolicyView: UIView {
+enum SearchPolicySection {
+    case mainSection
+}
+
+struct SearchPolicyItem: Hashable {
+    let uuid = UUID()
+    let id: Int
+    let policyTitle: String
+}
+
+final class SearchPolicyView: UIView, UITableViewDelegate {
+    private lazy var disposeBag = DisposeBag()
+
+    private lazy var viewModel = ResultPolicyViewModel(type: PolicyCategory.allCases,
+                                                       policyUseCase: PolicyUseCaseImpl(policyRepository: PolicyRepositoryImpl()))
+    
+    private var dataSource: UITableViewDiffableDataSource<SearchPolicySection, SearchPolicyItem>!
     
     private lazy var containerView = UIView().then {
         $0.backgroundColor = .white
@@ -34,7 +51,11 @@ final class SearchPolicyView: UIView {
     
     private lazy var searchIconImageView = UIImageView(image: UIImage(named: "magnifyingglass"))
     
-    private lazy var policyCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    private lazy var policyTableView = UITableView(frame: .zero, style: .plain).then {
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "sampleIdentifier")
+        $0.backgroundColor = .white
+        $0.prefetchDataSource = self
+    }
     
     private lazy var addButton = UILabel().then {
         $0.backgroundColor = .lime40
@@ -54,6 +75,41 @@ final class SearchPolicyView: UIView {
         
         layout()
         addTapEvents()
+        
+        dataSource = UITableViewDiffableDataSource<SearchPolicySection, SearchPolicyItem>(tableView: policyTableView, cellProvider: { tableView, indexPath, itemIdentifier in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "sampleIdentifier", for: indexPath)
+            
+            let bgColorView = UIView()
+            bgColorView.backgroundColor = .lime20
+            
+            cell.selectedBackgroundView = bgColorView
+            cell.textLabel?.text = itemIdentifier.policyTitle
+            cell.textLabel?.textColor = .black
+            cell.backgroundColor = .white
+            
+            return cell
+        })
+        
+        dataSource.defaultRowAnimation = .fade
+        
+        policyTableView.dataSource = dataSource
+        
+        self.viewModel.output.searchListRelay
+            .subscribe(onNext: { [weak self] items in
+                guard let self else { return }
+                
+                let beforeItems = dataSource.snapshot().itemIdentifiers
+                let itemList: [SearchPolicyItem] = items.map { SearchPolicyItem(id: Int($0.policy?.policyId ?? "0") ?? 0, policyTitle: $0.policy?.title ?? "") }
+                var snapshot = NSDiffableDataSourceSnapshot<SearchPolicySection, SearchPolicyItem>()
+                
+                snapshot.appendSections([.mainSection])
+                snapshot.appendItems(beforeItems + itemList, toSection: .mainSection)
+
+                dataSource.apply(snapshot, animatingDifferences: true)
+            })
+            .disposed(by: disposeBag)
+        
+        self.viewModel.fetchSearchList.accept(())
     }
     
     required init?(coder: NSCoder) {
@@ -71,7 +127,7 @@ final class SearchPolicyView: UIView {
         containerView.addSubview(titleLabel)
         containerView.addSubview(closeButton)
         containerView.addSubview(textFieldBackgroundView)
-        containerView.addSubview(policyCollectionView)
+        containerView.addSubview(policyTableView)
         containerView.addSubview(addButton)
         
         textFieldBackgroundView.addSubview(policyTextField)
@@ -113,8 +169,7 @@ final class SearchPolicyView: UIView {
             $0.centerY.equalToSuperview()
         }
         
-        policyCollectionView.backgroundColor = .red
-        policyCollectionView.snp.makeConstraints {
+        policyTableView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.top.equalTo(textFieldBackgroundView.snp.bottom).offset(20)
             $0.bottom.equalTo(addButton.snp.top).offset(-20)
@@ -125,6 +180,18 @@ final class SearchPolicyView: UIView {
             $0.leading.trailing.equalToSuperview().inset(27)
             $0.height.equalTo(50)
             $0.centerX.equalToSuperview()
+        }
+    }
+}
+
+extension SearchPolicyView: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        let total = self.dataSource.snapshot().itemIdentifiers(inSection: .mainSection).count
+        let currentPage = (total / 10) + 1
+        
+        // 끝에서 5개의 아이템 이내일 경우 다음 페이지 로드 요청
+        if let max = indexPaths.map({ $0.item }).max(), max >= total - 2 {
+            viewModel.input.pageUpdate.accept(currentPage)
         }
     }
 }
