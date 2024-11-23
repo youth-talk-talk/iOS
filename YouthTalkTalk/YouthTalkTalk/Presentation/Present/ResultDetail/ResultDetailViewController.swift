a59afcc77dbf3b5be332f5400c4deedf9af3a431
yououjin <71479613+Y00ujin@users.noreply.github.com>
2024-11-23 20:27:04 +0900

    feature: 게시글 수정, 삭제 기능 준비

--- YouthTalkTalk/YouthTalkTalk/Presentation/Present/ResultDetail/ResultDetailViewController.swift
+++ YouthTalkTalk/YouthTalkTalk/Presentation/Present/ResultDetail/ResultDetailViewController.swift
@@ -12,6 +12,32 @@ import RxSwift
 import RxCocoa
 
 class ResultDetailViewController: BaseViewController<ResultDetailView> {
+    // TODO: 게시글 생성, 삭제 기능 추가
+    private lazy var moreImageView = UIImageView(image: UIImage(named: "more"))
+    
+    private lazy var moreContainerStackView = UIStackView(arrangedSubviews: [moreEditLabel,
+                                                                             moreCenterLineView,
+                                                                             moreDeleteLabel]).then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 10
+        $0.layer.borderColor = FontColor.gray30.value.cgColor
+        $0.layer.borderWidth = 1
+        $0.isHidden = true
+    }
+    
+    private lazy var moreEditLabel = UILabel().then {
+        $0.designed(text: "수정", fontType: .p16Regular16)
+        $0.textAlignment = .center
+    }
+    
+    private lazy var moreCenterLineView = UIView().then {
+        $0.backgroundColor = FontColor.gray30.value
+    }
+        
+    private lazy var moreDeleteLabel = UILabel().then {
+        $0.designed(text: "삭제", fontType: .p16Regular16)
+        $0.textAlignment = .center
+    }
     
     private let viewModel: ResultDetailInterface
     
